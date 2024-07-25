238851c604583a8394f7dabbf5afa0636f7b30b2
Alexey Kogan <alekseikogan@yandex.ru>
2024-07-25 18:22:32 +0300

    настроил poetry, исправить баги

--- core/models/base.py
+++ core/models/base.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-
     @declared_attr.directive
-    def __tablename__(cls):
-        return f'{cls.__tablename__.lower()}s'
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from users.views import router as users_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.bigin() as conn:
+    async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
 
